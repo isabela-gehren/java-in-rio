c1a637ab661c7e26143cbc2fcbce899f84951925
Isabela <Isabela@isabela>
2016-04-03 17:23:53 -0300

    Exibe a programação

--- src/br/com/javainrio/facade/PalestraFacade.java
+++ src/br/com/javainrio/facade/PalestraFacade.java
@@ -1,10 +1,15 @@
 package br.com.javainrio.facade;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
 import br.com.javainrio.dao.PalestraDAO;
+import br.com.javainrio.entidade.Palestra;
 
 @Remote
 public interface PalestraFacade extends PalestraDAO {
+
+	List<Palestra> listarDoAnoCorrente();
 
 }
